25a938827a22ddec535e970a2ad2bfa0cff204bf
Firmanwijaya <firmanwijayadinata90@gmail.com>
2022-10-12 00:22:08 +0700

    run deploy

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home";
+import { Provider } from "react-redux";
+import store from "./bootstrap/store";
+
+function App() {
+  return (
+    <div className="App">
+      <Provider store={store}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/task_two" element={<HomePage />}></Route>
+            </Routes>
+          </BrowserRouter>
+        </Provider>
+    </div>
+  );
+}
+
+export default App;
